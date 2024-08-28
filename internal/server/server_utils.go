2e384137948baaef2565ded45c6ce908acd73c5a
Boris Terzic <boris@terzic.be>
2024-08-28 09:18:28 +0200

    Fix error in slog statement

--- internal/server/server_utils.go
+++ internal/server/server_utils.go
@@ -1,11 +1,12 @@
 package server
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func sendInternalError(c echo.Context, err error) error {
-	logger.Error("Error processing request: ", err)
+	logger.Error("Error processing request", "error", err)
 	return c.Render(http.StatusInternalServerError, "error-internalserver", nil)
 }
