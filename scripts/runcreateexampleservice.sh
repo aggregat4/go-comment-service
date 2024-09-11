3d154eeb8653a4e9e5e4584d8c5066a7d5ef4f0b
Boris Terzic <boris@terzic.be>
2024-09-11 09:45:59 +0200

    fix: uncomment service creation code in helper script

--- scripts/runcreateexampleservice.sh
+++ scripts/runcreateexampleservice.sh
@@ -2,4 +2,4 @@
 ENCRYPTION_KEY=$(go run cmd/createencryptionkey/main.go 2>&1)
 echo "ENCRYPTION_KEY: $ENCRYPTION_KEY"
 # echo go run cmd/createservice/main.go -db ~/tmp/commentservice/commentservice.sqlite -servicekey foobar -serviceorigin http://localhost:1234 -encryptionkey $ENCRYPTION_KEY
-# go run cmd/createservice/main.go -db ~/tmp/commentservice/commentservice.sqlite -servicekey foobar -serviceorigin http://localhost:1234 -encryptionkey "$ENCRYPTION_KEY"
+go run cmd/createservice/main.go -db ~/tmp/commentservice/commentservice.sqlite -servicekey foobar -serviceorigin http://localhost:1234 -encryptionkey "$ENCRYPTION_KEY"
