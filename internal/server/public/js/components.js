2ca33960acad6493c4353a3b697aea06dc289678
Boris Terzic <boris@terzic.be>
2024-10-16 10:09:19 +0200

    feature: improve the styling of the action buttons to make sure it is clear they belong together and that the widths are such that one does not accidentally trigger the action

--- internal/server/public/js/components.js
+++ internal/server/public/js/components.js
@@ -22,16 +22,31 @@ class ActionConfirmation extends HTMLElement {
     const payloadContent = this.getAttribute('payloadContent');
     const actionName = this.getAttribute('actionName');
     const actionUrl = this.getAttribute('actionUrl');
+    // We calculate the minimum width of the action button based on the action name length to make the form layout
+    // We need this to make sure that when we click on the confirm button, the cursor is not accidentally over the actual action but always over cancel
+    const actionNameNumChars = actionName.length;
+    
 
     this.shadowRoot.innerHTML = `
       <style>
         .hidden { display: none; }
+        .action-form {
+          display: flex;
+          gap: 1rem;
+          flex-direction: row;
+          background-color: #d5d5d5;
+          padding: 4px;
+          border-radius: 3px;
+        }
+        button {
+          min-width: ${actionNameNumChars}em;
+        }
       </style>
-      <button class="confirm">${actionName}...</button>
-      <button class="cancel hidden">Cancel</button>
-      <form class="action-form hidden" method="post" action="${actionUrl}">` +
+      <form class="action-form" method="post" action="${actionUrl}">
+        <button class="confirm">${actionName}...</button>
+        <button class="cancel hidden">Cancel</button>` +
         ((payloadName && payloadContent) ? `<input type="hidden" name="${payloadName}" value="${payloadContent}"/>` : ``) +
-`        <button class="action">${actionName}!</button>
+`       <button class="action hidden">${actionName}!</button>
       </form>`;
   }
 
@@ -41,32 +56,34 @@ class ActionConfirmation extends HTMLElement {
   addEventListeners() {
     const confirmButton = this.shadowRoot.querySelector('.confirm');
     const cancelButton = this.shadowRoot.querySelector('.cancel');
-    const actionForm = this.shadowRoot.querySelector('.action-form');
+    const actionButton = this.shadowRoot.querySelector('.action');
 
-    this.#confirmListener = () => {
+    this.#confirmListener = (e) => {
+      e.preventDefault();
       confirmButton.classList.add('hidden');
       cancelButton.classList.remove('hidden');
-      actionForm.classList.remove('hidden');
+      actionButton.classList.remove('hidden');
     };
     confirmButton.addEventListener('click', this.#confirmListener);
 
-    this.#cancelListener = () => {
+    this.#cancelListener = (e) => {
+      e.preventDefault();
       confirmButton.classList.remove('hidden');
       cancelButton.classList.add('hidden');
-      actionForm.classList.add('hidden');
+      actionButton.classList.add('hidden');
     };
     cancelButton.addEventListener('click', this.#cancelListener);
   }
 
   removeEventListeners() {
     if (this.#confirmListener != null) {
-        const confirmButton = this.shadowRoot.querySelector('.confirm');
-        confirmButton.removeEventListener('click', this.#confirmListenerlistener);
-        this.#confirmListener = null;
+      const confirmButton = this.shadowRoot.querySelector('.confirm');
+      confirmButton.removeEventListener('click', this.#confirmListener);
+      this.#confirmListener = null;
     }
     if (this.#cancelListener != null) {
       const cancelButton = this.shadowRoot.querySelector('.cancel');
-      cancelButton.removeEventListener('click', this.#cancelListenerlistener);
+      cancelButton.removeEventListener('click', this.#cancelListener);
       this.#cancelListener = null;
     }
   }
