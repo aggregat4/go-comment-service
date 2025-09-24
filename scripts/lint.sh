30426f63b492e679c32fa3e29b3458e927ace893
Boris Terzic <boris@terzic.be>
2025-09-24 09:26:34 +0200

    Improve build scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo Running golanci-lint
 "$(go env GOPATH)"/bin/golangci-lint run
+echo Running modernize
+go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest  -v -test ./...
